ddef36fb598b7c1ced5a51cda7dc8a3513429ad3
韶璠张 <shaofanzhang@shaofanzhangs-MacBook-Pro.local>
2015-12-03 19:19:03 -0500

    min problem fixed

--- src/main/java/org/salarymaster/util/Statistic.java
+++ src/main/java/org/salarymaster/util/Statistic.java
@@ -5,6 +5,7 @@
  */
 package org.salarymaster.util;
 
+import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.List;
 import org.salarymaster.model.Salary;
@@ -19,34 +20,40 @@ public class Statistic {
     //Get Min, Max, and Median value of Salary
     public static Stat getStatisticInfo(final List<Salary> salary) {
 //        Double[] res = new Double[3];
-        Stat res = new Stat(); 
-        if(salary.size() == 1){
+        Stat res = new Stat();
+        if (salary.size() == 1) {
             double max = salary.get(0).getWageOfferFrom9089();
-                        double median = salary.get(0).getWageOfferFrom9089();
+            double median = salary.get(0).getWageOfferFrom9089();
 
-                                    double min = salary.get(0).getWageOfferFrom9089();
+            double min = salary.get(0).getWageOfferFrom9089();
 
             res.setSalaryMin(min);
-        res.setSalaryMax(max);
-        res.setSalaryMedian(median);
-        return res;
-        }               
+            res.setSalaryMax(max);
+            res.setSalaryMedian(median);
+            return res;
+        }
         Collections.sort(salary, null);
         double max = salary.get(0).getWageOfferFrom9089();
         double min = 0;
-        for (int i = salary.size() - 1; i > -1; i--) {
+        if(max<10000){
+            min = salary.get(salary.size()-1).getWageOfferFrom9089();
+        }else{
+           for (int i = salary.size() - 1; i > -1; i--) {
             if (salary.get(i).getWageOfferFrom9089() >= 1000) {
                 min = salary.get(i).getWageOfferFrom9089();
                 break;
-            }  
+            }
         }
-
+        }
         double median = 0;
+        DecimalFormat df = new DecimalFormat("#.0");
         if (salary.size() % 2 == 0) {
             median = 0.5 * (salary.get(salary.size() / 2 - 1).getWageOfferFrom9089() + salary.get(salary.size() / 2).getWageOfferFrom9089());
         } else {
             median = salary.get(salary.size() / 2).getWageOfferFrom9089();
         }
+        median = Double.valueOf(df.format(median));
+        
         res.setSalaryMin(min);
         res.setSalaryMax(max);
         res.setSalaryMedian(median);
